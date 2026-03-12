331dbd4609b0b45243b8309a1f03eba2ce797553
Parth <parthagre84@gmail.com>
2026-03-12 15:40:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,redirect,url_for,request,flash
+from httpcore import __name
+
+app = Flask(__name__)
+
+# Secret key is required to enable sessions and flash messages in Flask
+app.secret_key = "mysecretkey"
+
+@app.route("/",methods=['GET','POST'])
+def main():
+    if request.method == 'POST':
+        flash("Login Successful", "success")# Flash is used to show temporary messages on a web page using session storage.
+        return redirect(url_for("home"))
+    return render_template("main.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)#we use the debug true because when an error comes so it will show inside the web browser 
