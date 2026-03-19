5b132a9560d21f300c2f9f537fbd46cb908681f9
Parth <parthagre84@gmail.com>
2026-03-19 21:57:46 +0530

    adding the addbusiness option html and css files and modifying the app.py add the search logic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,render_template,redirect,url_for,request,flash,session
 from httpcore import __name
 from flask_mysqldb import MySQL
+import os 
+from werkzeug.utils import secure_filename 
 
-
+#craeting the obj of flask class and save into app or we also say creating the flask app 
 app = Flask(__name__)
 
 # Secret key is required to enable sessions and flash messages in Flask
@@ -15,6 +17,11 @@ app.config['MYSQL_PASSWORD'] = '123456789'  # your password
 app.config['MYSQL_DB'] = 'user'      # your DB name
 
 mysql = MySQL(app)
+
+# creating the upload folder (folder path config)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 
 @app.route("/",methods=['GET','POST'])
@@ -65,10 +72,16 @@ def register():
         return redirect("/home")
     return render_template("register.html")
 
+
+@app.route("/owner_dashboard")
+def owner_dashboard():
+    return render_template("dashboard.html", username=session['username'],name = name , location=location, description = description , category = category)
+
+
+
 @app.route("/home")
 def home():
     return render_template("home.html",username = session['username'])
-
 
 
 @app.route('/customer_dashboard')
@@ -78,6 +91,35 @@ def customer_dashboard():
 
     return render_template('customer_dashboard.html', username=session['username'])
 
+
+
+@app.route("/addbusiness" , methods = ["GET","POST"])
+def addbusiness():
+    if request.method == 'POST':
+        #getting the form section details 
+        name = request.form.get("name")
+        location = request.form.get("location")
+        description = request.form.get("description")
+        category = request.form.get("category")
+
+        #getting the files uploaded by the user 
+        image = request.files['image']
+
+        if image and image.filename != "":
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'] ,filename))
+
+        else:
+            filename = 'default.jpg'
+
+        cur = mysql.connection.cursor()
+        cur.execute(f"INSERT INTO business_record (name,description,location,category,images) VALUES ('{name}','{description}','{location}','{category}','{filename}')")
+        mysql.connection.commit()
+        cur.close()
+
+        # return redirect(url_for('owner_dashboard',name = name , location=location, description = description , category = category))
+        return render_template("dashboard.html"session['username'] = username,name = name , location=location, description = description , category = category)
+    return render_template("addbusiness.html")
 
 
 @app.route('/logout')
